c98fc012e0cfe4f0f40b9fda0d5bb74b98db0d42
Kashif Hussain <kashif.hussain@enthral.ai>
2025-05-26 20:11:31 +0530

    Initial Commit

---
+++ DownloadManagerDemoTests/DownloadManagerDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadManagerDemoTests.swift
+//  DownloadManagerDemoTests
+//
+//  Created by Kashif Hussain on 26/05/25.
+//
+
+import Testing
+@testable import DownloadManagerDemo
+
+struct DownloadManagerDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
